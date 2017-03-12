a108a934f6b23fe53a2e0333be2f137a768e4830
Lana <lana@macpro.local>
2017-03-12 14:58:58 -0400

    Adding comments

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,39 +2,31 @@ var database = firebase.database();
 
 angular.module('app.controllers', [])
 
-.controller('homeCtrl', ['$scope', '$stateParams', '$location',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
+.controller('homeCtrl', ['$scope', '$stateParams', '$location',
 function ($scope, $stateParams, $location) {
+  // When anybody is in the Home page it automatically signs them out
   if (firebase.auth().currentUser) {
     firebase.auth().signOut();
   }
 }])
 
-.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
-
-}])
-
-.controller('findPetsNearMeCtrl', ['$scope', '$stateParams', '$location',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
+.controller('findPetsNearMeCtrl', ['$scope', '$stateParams', '$location',
 function ($scope, $stateParams, $location) {
   var database = firebase.database();
   var markers = [];
 
+  // Shows Jacksonville on the map
   var center = {lat: 30.34, lng: -81.51};
   var map = new google.maps.Map(document.getElementById('map'), {zoom: 13, center: center});
   var geocoder = new google.maps.Geocoder();
 
+  // Retrieve all the pets in Firebase
   database.ref('pet').on('value', function(snapshot){
     $scope.$evalAsync(function(){
       $scope.pets = snapshot.val();
     });
 
+    // Clear the map as part of a full refresh
     for(var i = 0; i < markers.length; i++) {
       markers[i].setMap(null);
     }
@@ -42,15 +34,20 @@ function ($scope, $stateParams, $location) {
 
     for (var pet in snapshot.val()){
       var petData = snapshot.val()[pet];
+      // Retrieve data for pet owner
       database.ref('petOwner/' + petData.owner).once("value").then(function(owner){
+        // Get latitude and longitude for the pet owner's address
         geocoder.geocode({"address": owner.val().address + ' ' + owner.val().zipCode},
           function(results, status) {
+            // Put pet marker on map
             var marker = new google.maps.Marker({
               position: results[0].geometry.location,
               map: map,
+              // Use the type of animal (cat or dog) to choose which icon is displayed
               icon: "https://s3.amazonaws.com/fphp/" + this.petData.petType + ".png"
             });
 
+            // When you click on cat or dog icon, show pet profile
             marker.addListener('click', function(){
               $scope.$apply(function(){
                 console.log(this.pet);
@@ -58,6 +55,7 @@ function ($scope, $stateParams, $location) {
               }.bind({pet: this.pet}));
             }.bind({pet: this.pet}));
 
+            // Add marker to list to use when refreshing
             markers.push(marker);
           }.bind({petData: this.petData, pet: this.pet}));
       }.bind({petData: petData, pet: pet}));
@@ -67,13 +65,14 @@ function ($scope, $stateParams, $location) {
 }])
 
 .controller('petOwnerSignupCtrl',
-  ['$scope', '$stateParams', '$location',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-    // You can include any angular dependencies as parameters for this function
-    // TIP: Access Route Parameters for your page via $stateParams.parameterName
+  ['$scope', '$stateParams', '$location',
     function ($scope, $stateParams, $location) {
       $scope.signUp = function(data){
         console.log("signUp");
+
+        // Create user in Firebase
         firebase.auth().createUserWithEmailAndPassword(data.email, data.password).then(function(user){
+          // Store pet owner data
           database.ref("petOwner/" + user.uid).set({
             email: data.email,
             name: data.name,
@@ -92,16 +91,18 @@ function ($scope, $stateParams, $location) {
   ]
 )
 
-.controller('loginCtrl', ['$scope', '$stateParams', '$location', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
+.controller('loginCtrl', ['$scope', '$stateParams', '$location',
 function ($scope, $stateParams, $location) {
   $scope.login = function(crendentials){
     console.log("login");
+    // Sign in user in Firebase
     firebase.auth().signInWithEmailAndPassword(crendentials.email, crendentials.password).then(function(user){
+      // Try to find the current user as a pet owner
       database.ref("petOwner/" + user.uid).once("value").then(function(owner){
+        // If the current user is not a pet owner then it is a seeker
         if (owner.val() == null) {
           $scope.$apply(function() {
+            // Go to Find Pets Near Me
             $location.path('/page4');
           });
         } else {
@@ -124,17 +125,17 @@ function ($scope, $stateParams) {
 
 }])
 
-.controller('petOwnerSignup2Ctrl', ['$scope', '$stateParams', '$location', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
+.controller('petOwnerSignup2Ctrl', ['$scope', '$stateParams', '$location',
 function ($scope, $stateParams, $location) {
 
   var type = 'cat';
+  // Change button color to blue and tan
   document.getElementById('dog-button').className = 'button button-loyal  button-block';
   document.getElementById('cat-button').className = 'button button-positive  button-block';
 
   $scope.signUp = function(data){
     console.log("signUp");
+    //
     var pet = database.ref("pet").push();
     pet.set({
       petType: type,
@@ -182,7 +183,9 @@ function ($scope, $stateParams) {
 function ($scope, $stateParams, $location) {
   $scope.signUp = function(data){
     console.log("signUp");
+    // Create user in Firebase
     firebase.auth().createUserWithEmailAndPassword(data.email, data.password).then(function(user){
+      // Get seeker data from Firebase
       database.ref("petSeeker/" + user.uid).set({
         email: data.email,
         name: data.name,
@@ -199,9 +202,7 @@ function ($scope, $stateParams, $location) {
 
 }])
 
-.controller('myProfileCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
+.controller('myProfileCtrl', ['$scope', '$stateParams',
 function ($scope, $stateParams) {
 
 
@@ -224,6 +225,7 @@ function ($scope, $stateParams, $location) {
   })
 
   $scope.requestVisit = function() {
+    // Find who requested your pet and which pet (if you have more than one pet in Firebase)
     var request = database.ref("request/" + $scope.petOwner).push();
     request.set({
       seeker: firebase.auth().currentUser.uid,
@@ -266,9 +268,7 @@ function ($scope, $stateParams) {
 
 }])
 
-.controller('petRequestsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
+.controller('petRequestsCtrl', ['$scope', '$stateParams',
 function ($scope, $stateParams) {
   database.ref('request/' + firebase.auth().currentUser.uid).on("value", function(snapshot){
     $scope.$evalAsync(function(){
@@ -278,6 +278,7 @@ function ($scope, $stateParams) {
           $scope.requests[this.request].petName = pet.val().petName;
         }.bind({request: request}));
 
+        // Make the seeker id readable
         database.ref('petSeeker/' + $scope.requests[request].seeker).once("value").then(function(seeker){
           $scope.requests[this.request].seekerName = seeker.val().name;
           $scope.requests[this.request].seekerEmail = seeker.val().email;
